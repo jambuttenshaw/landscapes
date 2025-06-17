c0d3201e2dd929459dbf05994d3028f8637d944c
jambuttenshaw <jamie.buttenshaw@gmail.com>
2025-06-17 12:20:45 +0100

    Improving CMake setup

---
+++ source/main.cpp
@@ -0,0 +1,45 @@
+#include <donut/app/ApplicationBase.h>
+#include <donut/engine/ShaderFactory.h>
+#include <donut/app/DeviceManager.h>
+#include <donut/core/log.h>
+#include <nvrhi/utils.h>
+
+#include "basic_triangle.h"
+
+#ifdef WIN32
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+#else
+int main(int __argc, const char** __argv)
+#endif
+{
+    nvrhi::GraphicsAPI api = app::GetGraphicsAPIFromCommandLine(__argc, __argv);
+    app::DeviceManager* deviceManager = app::DeviceManager::Create(api);
+
+    app::DeviceCreationParameters deviceParams;
+#ifdef _DEBUG
+    deviceParams.enableDebugRuntime = true; 
+    deviceParams.enableNvrhiValidationLayer = true;
+#endif
+
+    if (!deviceManager->CreateWindowDeviceAndSwapChain(deviceParams, g_WindowTitle))
+    {
+        log::fatal("Cannot initialize a graphics device with the requested parameters");
+        return 1;
+    }
+    
+    {
+        BasicTriangle example(deviceManager);
+        if (example.Init())
+        {
+            deviceManager->AddRenderPassToBack(&example);
+            deviceManager->RunMessageLoop();
+            deviceManager->RemoveRenderPass(&example);
+        }
+    }
+    
+    deviceManager->Shutdown();
+
+    delete deviceManager;
+
+    return 0;
+}
